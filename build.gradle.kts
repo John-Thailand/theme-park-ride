0034683783180660d3644bdfcdb0b8f658271a88
John-Thailand <naoki.honda@mirrorfit.jp>
2026-06-28 12:46:35 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Gradleが内部で利用しているAntというビルドツールのクラス
+// GradleにはAntライブラリが最初から含まれています。
+import org.apache.tools.ant.filters.ReplaceTokens
+
+plugins {
+  base
+}
+
+tasks.register<Copy>("generateDescriptions") {
+  from("descriptions")
+  into(layout.buildDirectory.dir("descriptions"))
+  filter<ReplaceTokens>("tokens" to mapOf("THEME_PARK_NAME" to "Grelephant's Wonder World"))
+}
